1d9840a5fc08672d4a3cb4ebe9710f68af51ffca
Lu Abikoye <luabikoye@yahoo.com>
2025-05-07 09:54:02 +0100

    Updated script

--- connect.php
+++ connect.php
@@ -1,5 +1,11 @@
 <?php
-@ $db = mysqli_connect('wal-db.c3w2g26cei13.eu-north-1.rds.amazonaws.com', 'wal_db', 'Aledoy101','wal_db');
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+if($_SERVER['HTTP_HOST'] != 'localhost:8888')
+{
+  @ $db = mysqli_connect('wal-db.c3w2g26cei13.eu-north-1.rds.amazonaws.com', 'wal_db', 'Aledoy101','wal_db');
 
   if (!$db)
   {
@@ -7,22 +13,16 @@
      exit;
   }
 
-
-
-/*@ $db = mysql_pconnect('localhost', 'gxxieedd_mydb', 'certification231');
+}else
+{
+@ $db = mysqli_connect('localhost', 'mccljxii_ijoba', 'certification231','mccljxii_ijoba');
 
   if (!$db)
   {
      echo 'Error: Could not connect to host server.  Please try again later.';
      exit;
   }
-
-  $mysql = mysql_select_db('gxxieedd_mydb');
-    if (!$mysql)
-  {
-     echo 'Error: Could not Select database.  Please try again later.';
-     exit;
-  }*/
+}
 
 
 function c_date($date,$return)
